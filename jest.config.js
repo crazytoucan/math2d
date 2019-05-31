b99f4b7d3fec6080b08cac21009a676e87c2a008
Alex Ryan <alexanderfryan@gmail.com>
2019-05-31 14:27:13 -0700

    Add jest testing infrastructure

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  moduleFileExtensions: ["ts", "tsx", "js"],
+  testRegex: "\\.(test|spec)\\.tsx?$",
+  transform: {
+    "\\.(js|jsx|ts|tsx)$": "ts-jest",
+  },
+  transformIgnorePatterns: ["/node_modules/(?!(lodash-es|@foundry|@workshop|@palantir|@acme|@gotham|@tron)/)"],
+  globals: {
+    "ts-jest": {
+      tsConfigFile: "tsconfig.jest.json",
+    },
+  },
+  verbose: true,
+  reporters: ["default"],
+};
