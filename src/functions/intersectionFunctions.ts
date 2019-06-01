a9e86766311642b5f044690f94043b542193183a
Alex Ryan <alexanderfryan@gmail.com>
2019-06-01 15:34:53 -0700

    Add intersectionFunctions tsdoc

--- src/functions/intersectionFunctions.ts
+++ src/functions/intersectionFunctions.ts
@@ -1,14 +1,33 @@
 import { _intersectionAlloc } from "../internal/dataClasses";
 import { IIntersection } from "../types";
 
+/**
+ * Creates a new Intersection object in memory, with all values initialized to `false` and `NaN`.
+ * This is useful to hold the result of vectormath function calls in performance
+ * critical workflows.
+ */
 export function intersectionAlloc() {
   return _intersectionAlloc();
 }
 
-export function intersectionClone(inter: IIntersection, out = intersectionAlloc()) {
-  return intersectionReset(inter.exists, inter.x, inter.y, inter.t0, inter.t1, out);
+/**
+ * Copies the values from the given intersection into a new intersection object.
+ * @param intersection
+ * @param out
+ */
+export function intersectionClone(intersection: IIntersection, out = intersectionAlloc()) {
+  return intersectionReset(intersection.exists, intersection.x, intersection.y, intersection.t0, intersection.t1, out);
 }
 
+/**
+ * Constuct a new intersection given `exists`, `x`, `y`, `t0`, and `t1` values.
+ * @param exists
+ * @param x
+ * @param y
+ * @param t0
+ * @param t1
+ * @param out
+ */
 export function intersectionReset(
   exists: boolean,
   x: number,
