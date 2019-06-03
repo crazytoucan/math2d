e80d673e823ff44fc5c99c199bee3fcae90adddf
Alex Ryan <alexanderfryan@gmail.com>
2019-06-03 12:33:44 -0700

    Add dedicated API reference template for each kind of exported symbol

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,6 @@
 // tslint:disable:no-bitwise
-export const OUT_MIN_X = 1 << 0;
-export const OUT_MAX_X = 1 << 1;
-export const OUT_MIN_Y = 1 << 2;
-export const OUT_MAX_Y = 1 << 3;
+export const OUT_MIN_X = 1;
+export const OUT_MAX_X = 2;
+export const OUT_MIN_Y = 4;
+export const OUT_MAX_Y = 8;
 // tslint:enable:no-bitwise
