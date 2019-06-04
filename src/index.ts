09426e68213029866543b9fec613755417c6d1ef
Alex Ryan <alexanderfryan@gmail.com>
2019-06-04 13:26:11 -0700

    Update package index per new file structure

--- src/index.ts
+++ src/index.ts
@@ -1,139 +1,123 @@
+export { boxAlloc } from "./boxFunctions/boxAlloc";
+export { boxClone } from "./boxFunctions/boxClone";
+export { boxComputeOutCode } from "./boxFunctions/boxComputeOutCode";
+export { boxContainsBox } from "./boxFunctions/boxContainsBox";
+export { boxContainsPoint } from "./boxFunctions/boxContainsPoint";
+export { boxEncapsulate } from "./boxFunctions/boxEncapsulate";
+export { boxIntersection } from "./boxFunctions/boxIntersection";
+export { boxIntersectsBox } from "./boxFunctions/boxIntersectsBox";
+export { boxIsEmpty } from "./boxFunctions/boxIsEmpty";
+export { boxReset } from "./boxFunctions/boxReset";
+export { boxTransformByAff } from "./boxFunctions/boxTransformByAff";
+export { boxUnion } from "./boxFunctions/boxUnion";
 export { OUT_MAX_X, OUT_MAX_Y, OUT_MIN_X, OUT_MIN_Y } from "./const";
-export {
-  boxAlloc,
-  boxClone,
-  boxComputeOutCode,
-  boxContainsBox,
-  boxContainsPoint,
-  boxEncapsulate,
-  boxIntersection,
-  boxIntersectsBox,
-  boxIsEmpty,
-  boxReset,
-  boxTransformByAff,
-  boxUnion,
-} from "./functions/boxFunctions";
-export {
-  lineAlloc,
-  lineClone,
-  lineClosestDistanceToPoint,
-  lineClosestSignedDistanceToPoint,
-  lineContainsPoint,
-  lineIntersectLine,
-  lineIntersectPolylineIterator,
-  lineIntersectRay,
-  lineIntersectSegment,
-  lineLookAt,
-  linePointAt,
-  lineProjectPoint,
-  lineReset,
-  lineSide,
-  lineTransformByAff,
-} from "./functions/lineFunctions";
-export {
-  mat2x3AffDeterminant,
-  mat2x3AffFromRotation,
-  mat2x3AffFromTranslation,
-  mat2x3AffIdentity,
-  mat2x3AffInvert,
-  mat2x3AffIsOrthogonal,
-  mat2x3AffIsTranslationOnly,
-  mat2x3AffMulMat2x3,
-  mat2x3AffRotate,
-  mat2x3AffScale,
-  mat2x3AffTranslate,
-  mat2x3Alloc,
-  mat2x3Clone,
-  mat2x3Reset,
-} from "./functions/mat2x3Functions";
-export {
-  polygonAlloc,
-  polygonContainsPoint,
-  polygonGetBounds,
-  polygonGetNumSides,
-  polygonGetPerimeter,
-  polygonGetSideLength,
-  polygonGetSideSegment,
-  polygonIntersectLineIterator,
-  polygonIntersectRayIterator,
-  polygonIntersectSegmentIterator,
-  polygonNearestPoint,
-  polygonNearestT,
-  polygonNearestVertexIndex,
-  polygonPointAt,
-  polygonSideIndexAt,
-  polygonTransformByAff,
-} from "./functions/polygonFunctions";
-export {
-  polylineAlloc,
-  polylineClose,
-  polylineGetBounds,
-  polylineGetLength,
-  polylineGetNumSegments,
-  polylineGetSegment,
-  polylineGetSegmentLength,
-  polylineGetVertex,
-  polylineIntersectLineIterator,
-  polylineIntersectRayIterator,
-  polylineIntersectSegmentIterator,
-  polylineIsClosed,
-  polylineNearestPoint,
-  polylineNearestT,
-  polylineNearestVertexIndex,
-  polylinePointAt,
-  polylineSegmentIndexAt,
-  polylineTransformByAff,
-  polylineTrim,
-} from "./functions/polylineFunctions";
-export {
-  rayAlloc,
-  rayClone,
-  rayContainsPoint,
-  rayGetClosestDistanceToPoint,
-  rayGetClosestPoint,
-  rayIntersectLine,
-  rayIntersectPolylineIterator,
-  rayIntersectRay,
-  rayIntersectSegment,
-  rayLookAt,
-  rayPointAt,
-  rayReset,
-  rayTransformByAff,
-} from "./functions/rayFunctions";
-export {
-  segmentAlloc,
-  segmentGetEndpoint0,
-  segmentGetEndpoint1,
-  segmentGetLength,
-  segmentGetLengthSq,
-  segmentIntersectLine,
-  segmentIntersectPolylineIterator,
-  segmentIntersectRay,
-  segmentIntersectSegment,
-  segmentNearestPoint,
-  segmentNearestT,
-  segmentPointAt,
-  segmentReset,
-  segmentReverse,
-} from "./functions/segmentFunctions";
-export {
-  vecAdd,
-  vecAlloc,
-  vecClone,
-  vecDistance,
-  vecDistanceSq,
-  vecDot,
-  vecGetLength,
-  vecGetLengthSq,
-  vecGetManhattanLength,
-  vecLerp,
-  vecManhattanDistance,
-  vecNormalize,
-  vecOrigin,
-  vecPerp,
-  vecReset,
-  vecScale,
-  vecSubtract,
-  vecTransformByAff,
-} from "./functions/vecFunctions";
+export { lineAlloc } from "./lineFunctions/lineAlloc";
+export { lineClone } from "./lineFunctions/lineClone";
+export { lineClosestDistanceToPoint } from "./lineFunctions/lineClosestDistanceToPoint";
+export { lineClosestSignedDistanceToPoint } from "./lineFunctions/lineClosestSignedDistanceToPoint";
+export { lineContainsPoint } from "./lineFunctions/lineContainsPoint";
+export { lineIntersectLine } from "./lineFunctions/lineIntersectLine";
+export { lineIntersectPolylineIterator } from "./lineFunctions/lineIntersectPolylineIterator";
+export { lineIntersectRay } from "./lineFunctions/lineIntersectRay";
+export { lineIntersectSegment } from "./lineFunctions/lineIntersectSegment";
+export { lineLookAt } from "./lineFunctions/lineLookAt";
+export { linePointAt } from "./lineFunctions/linePointAt";
+export { lineProjectPoint } from "./lineFunctions/lineProjectPoint";
+export { lineReset } from "./lineFunctions/lineReset";
+export { lineSide } from "./lineFunctions/lineSide";
+export { lineTransformByAff } from "./lineFunctions/lineTransformByAff";
+export { mat2x3AffDeterminant } from "./mat2x3Functions/mat2x3AffDeterminant";
+export { mat2x3AffFromRotation } from "./mat2x3Functions/mat2x3AffFromRotation";
+export { mat2x3AffFromTranslation } from "./mat2x3Functions/mat2x3AffFromTranslation";
+export { mat2x3AffIdentity } from "./mat2x3Functions/mat2x3AffIdentity";
+export { mat2x3AffInvert } from "./mat2x3Functions/mat2x3AffInvert";
+export { mat2x3AffIsOrthogonal } from "./mat2x3Functions/mat2x3AffIsOrthogonal";
+export { mat2x3AffIsTranslationOnly } from "./mat2x3Functions/mat2x3AffIsTranslationOnly";
+export { mat2x3AffMulMat2x3 } from "./mat2x3Functions/mat2x3AffMulMat2x3";
+export { mat2x3AffRotate } from "./mat2x3Functions/mat2x3AffRotate";
+export { mat2x3AffScale } from "./mat2x3Functions/mat2x3AffScale";
+export { mat2x3AffTranslate } from "./mat2x3Functions/mat2x3AffTranslate";
+export { mat2x3Alloc } from "./mat2x3Functions/mat2x3Alloc";
+export { mat2x3Clone } from "./mat2x3Functions/mat2x3Clone";
+export { mat2x3Reset } from "./mat2x3Functions/mat2x3Reset";
+export { polygonAlloc } from "./polygonFunctions/polygonAlloc";
+export { polygonContainsPoint } from "./polygonFunctions/polygonContainsPoint";
+export { polygonGetBounds } from "./polygonFunctions/polygonGetBounds";
+export { polygonGetNumSides } from "./polygonFunctions/polygonGetNumSides";
+export { polygonGetPerimeter } from "./polygonFunctions/polygonGetPerimeter";
+export { polygonGetSideLength } from "./polygonFunctions/polygonGetSideLength";
+export { polygonGetSideSegment } from "./polygonFunctions/polygonGetSideSegment";
+export { polygonIntersectLineIterator } from "./polygonFunctions/polygonIntersectLineIterator";
+export { polygonIntersectRayIterator } from "./polygonFunctions/polygonIntersectRayIterator";
+export { polygonIntersectSegmentIterator } from "./polygonFunctions/polygonIntersectSegmentIterator";
+export { polygonNearestPoint } from "./polygonFunctions/polygonNearestPoint";
+export { polygonNearestT } from "./polygonFunctions/polygonNearestT";
+export { polygonNearestVertexIndex } from "./polygonFunctions/polygonNearestVertexIndex";
+export { polygonPointAt } from "./polygonFunctions/polygonPointAt";
+export { polygonSideIndexAt } from "./polygonFunctions/polygonSideIndexAt";
+export { polygonTransformByAff } from "./polygonFunctions/polygonTransformByAff";
+export { polylineAlloc } from "./polylineFunctions/polylineAlloc";
+export { polylineClose } from "./polylineFunctions/polylineClose";
+export { polylineGetBounds } from "./polylineFunctions/polylineGetBounds";
+export { polylineGetLength } from "./polylineFunctions/polylineGetLength";
+export { polylineGetNumSegments } from "./polylineFunctions/polylineGetNumSegments";
+export { polylineGetSegment } from "./polylineFunctions/polylineGetSegment";
+export { polylineGetSegmentLength } from "./polylineFunctions/polylineGetSegmentLength";
+export { polylineGetVertex } from "./polylineFunctions/polylineGetVertex";
+export { polylineIntersectLineIterator } from "./polylineFunctions/polylineIntersectLineIterator";
+export { polylineIntersectRayIterator } from "./polylineFunctions/polylineIntersectRayIterator";
+export { polylineIntersectSegmentIterator } from "./polylineFunctions/polylineIntersectSegmentIterator";
+export { polylineIsClosed } from "./polylineFunctions/polylineIsClosed";
+export { polylineNearestPoint } from "./polylineFunctions/polylineNearestPoint";
+export { polylineNearestT } from "./polylineFunctions/polylineNearestT";
+export { polylineNearestVertexIndex } from "./polylineFunctions/polylineNearestVertexIndex";
+export { polylinePointAt } from "./polylineFunctions/polylinePointAt";
+export { polylineSegmentIndexAt } from "./polylineFunctions/polylineSegmentIndexAt";
+export { polylineTransformByAff } from "./polylineFunctions/polylineTransformByAff";
+export { polylineTrim } from "./polylineFunctions/polylineTrim";
+export { rayAlloc } from "./rayFunctions/rayAlloc";
+export { rayClone } from "./rayFunctions/rayClone";
+export { rayContainsPoint } from "./rayFunctions/rayContainsPoint";
+export { rayGetClosestDistanceToPoint } from "./rayFunctions/rayGetClosestDistanceToPoint";
+export { rayGetClosestPoint } from "./rayFunctions/rayGetClosestPoint";
+export { rayIntersectLine } from "./rayFunctions/rayIntersectLine";
+export { rayIntersectPolylineIterator } from "./rayFunctions/rayIntersectPolylineIterator";
+export { rayIntersectRay } from "./rayFunctions/rayIntersectRay";
+export { rayIntersectSegment } from "./rayFunctions/rayIntersectSegment";
+export { rayLookAt } from "./rayFunctions/rayLookAt";
+export { rayPointAt } from "./rayFunctions/rayPointAt";
+export { rayReset } from "./rayFunctions/rayReset";
+export { rayTransformByAff } from "./rayFunctions/rayTransformByAff";
+export { segmentAlloc } from "./segmentFunctions/segmentAlloc";
+export { segmentGetEndpoint0 } from "./segmentFunctions/segmentGetEndpoint0";
+export { segmentGetEndpoint1 } from "./segmentFunctions/segmentGetEndpoint1";
+export { segmentGetLength } from "./segmentFunctions/segmentGetLength";
+export { segmentGetLengthSq } from "./segmentFunctions/segmentGetLengthSq";
+export { segmentIntersectLine } from "./segmentFunctions/segmentIntersectLine";
+export { segmentIntersectPolylineIterator } from "./segmentFunctions/segmentIntersectPolylineIterator";
+export { segmentIntersectRay } from "./segmentFunctions/segmentIntersectRay";
+export { segmentIntersectSegment } from "./segmentFunctions/segmentIntersectSegment";
+export { segmentNearestPoint } from "./segmentFunctions/segmentNearestPoint";
+export { segmentNearestT } from "./segmentFunctions/segmentNearestT";
+export { segmentPointAt } from "./segmentFunctions/segmentPointAt";
+export { segmentReset } from "./segmentFunctions/segmentReset";
+export { segmentReverse } from "./segmentFunctions/segmentReverse";
 export { IBox, IIntersection, ILine, IMat2x3, IPolygon, IPolyline, IRay, ISegment, IVec } from "./types";
+export { vecAdd } from "./vecFunctions/vecAdd";
+export { vecAlloc } from "./vecFunctions/vecAlloc";
+export { vecClone } from "./vecFunctions/vecClone";
+export { vecDistance } from "./vecFunctions/vecDistance";
+export { vecDistanceSq } from "./vecFunctions/vecDistanceSq";
+export { vecDot } from "./vecFunctions/vecDot";
+export { vecGetLength } from "./vecFunctions/vecGetLength";
+export { vecGetLengthSq } from "./vecFunctions/vecGetLengthSq";
+export { vecGetManhattanLength } from "./vecFunctions/vecGetManhattanLength";
+export { vecLerp } from "./vecFunctions/vecLerp";
+export { vecManhattanDistance } from "./vecFunctions/vecManhattanDistance";
+export { vecNormalize } from "./vecFunctions/vecNormalize";
+export { vecOrigin } from "./vecFunctions/vecOrigin";
+export { vecPerp } from "./vecFunctions/vecPerp";
+export { vecReset } from "./vecFunctions/vecReset";
+export { vecScale } from "./vecFunctions/vecScale";
+export { vecSubtract } from "./vecFunctions/vecSubtract";
+export { vecTransformByAff } from "./vecFunctions/vecTransformByAff";
