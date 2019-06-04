54d1b2071050a12b18f8660c3b65fd9645c7c644
Alex Ryan <alexanderfryan@gmail.com>
2019-06-04 16:52:58 -0700

    Render tsdoc @links for shortTexts

--- docs/_plugins/jekyll-tsdoc.rb
+++ docs/_plugins/jekyll-tsdoc.rb
@@ -2,7 +2,7 @@ module Jekyll
   module TSDocFilter
     def tsdoc(input)
       site = @context.registers[:site]
-      input.gsub(/\{\@link\s+([^\}]+)\s*\}/, "[\\1](/#{site.config["baseurl"]}api/#\\1)")
+      input ? input.gsub(/\{\@link\s+([^\}]+)\s*\}/, "[\\1](/#{site.config["baseurl"]}api/#\\1)") : nil
     end
   end
 end
