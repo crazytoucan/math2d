cb9509b59a192093309721baea0c9bb69fb49851
Alex Ryan <alexanderfryan@gmail.com>
2019-06-05 13:32:10 -0700

    Improve strange mat2x3AffScale() behavior

--- src/__test__/mat2x3Functions/mat2x3AffScale.spec.ts
+++ src/__test__/mat2x3Functions/mat2x3AffScale.spec.ts
@@ -4,8 +4,8 @@ import { mat2x3Reset } from "../../mat2x3Functions/mat2x3Reset";
 import { expectMat2x3EqualsApprox } from "../helpers";
 
 describe("mat2x3AffScale", () => {
-  it("[1 2 3 4 5 6] scale 7 => [7 14 21 28 5 6]", () => {
-    expectMat2x3EqualsApprox(mat2x3AffScale(mat2x3Reset(1, 2, 3, 4, 5, 6), 7), mat2x3Reset(7, 14, 21, 28, 5, 6));
+  it("[1 2 3 4 5 6] scale 7 => [7 14 21 28 35 42]", () => {
+    expectMat2x3EqualsApprox(mat2x3AffScale(mat2x3Reset(1, 2, 3, 4, 5, 6), 7), mat2x3Reset(7, 14, 21, 28, 35, 42));
   });
 
   it("[1 0 0 1 0 0] scale NaN => [NaN NaN NaN NaN 0 0]", () => {
--- src/mat2x3Functions/mat2x3AffScale.ts
+++ src/mat2x3Functions/mat2x3AffScale.ts
@@ -2,6 +2,19 @@ import { IMat2x3 } from "../types";
 import { mat2x3Alloc } from "./mat2x3Alloc";
 import { mat2x3Reset } from "./mat2x3Reset";
 
-export function mat2x3AffScale(mat: IMat2x3, scalar: number, out = mat2x3Alloc()) {
-  return mat2x3Reset(scalar * mat.a, scalar * mat.b, scalar * mat.c, scalar * mat.d, mat.e, mat.f, out);
+/**
+ * Applies a scaling transform on top of the given affine matrix, returning the result.
+ *
+ * This is equivalent to _left_-multiplying the matrix by a scaling transform; that is,
+ * the result of this function is equivalent to a transform that first applies the matrix _mat_
+ * and then scales in both the x- and y-directions according to _scale_.
+ *
+ * @param mat the matrix to transform
+ * @param theta rotation angle in radians to apply on top of the given matrix
+ * @param out
+ * @see {@link mat2x3AffFromRotation}
+ * @see {@link mat2x3AffMulMat2x3Aff}
+ */
+export function mat2x3AffScale(mat: IMat2x3, scale: number, out = mat2x3Alloc()) {
+  return mat2x3Reset(scale * mat.a, scale * mat.b, scale * mat.c, scale * mat.d, scale * mat.e, scale * mat.f, out);
 }
