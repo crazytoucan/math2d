14d356c07409c4cc2cf0e71e5827e5ce76125a51
Alex Ryan <alexanderfryan@gmail.com>
2019-06-05 17:57:42 -0700

    Fix mat2x3AffScale test per new mat2x3AffScale behavior

--- src/__test__/mat2x3Functions/mat2x3AffScale.spec.ts
+++ src/__test__/mat2x3Functions/mat2x3AffScale.spec.ts
@@ -8,7 +8,7 @@ describe("mat2x3AffScale", () => {
     expectMat2x3EqualsApprox(mat2x3AffScale(mat2x3Reset(1, 2, 3, 4, 5, 6), 7), mat2x3Reset(7, 14, 21, 28, 35, 42));
   });
 
-  it("[1 0 0 1 0 0] scale NaN => [NaN NaN NaN NaN 0 0]", () => {
-    expectMat2x3EqualsApprox(mat2x3AffScale(mat2x3AffIdentity(), NaN), mat2x3Reset(NaN, NaN, NaN, NaN, 0, 0));
+  it("[1 0 0 1 0 0] scale NaN => [NaN NaN NaN NaN NaN NaN]", () => {
+    expectMat2x3EqualsApprox(mat2x3AffScale(mat2x3AffIdentity(), NaN), mat2x3Reset(NaN, NaN, NaN, NaN, NaN, NaN));
   });
 });
