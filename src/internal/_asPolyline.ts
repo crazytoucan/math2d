4e5addef05937704c16d1a8b9a2ea9a26769eb3b
Alex Ryan <alexanderfryan@gmail.com>
2020-05-25 01:14:10 -0700

    Remove Allocator usage (#19)

--- src/internal/_asPolyline.ts
+++ src/internal/_asPolyline.ts
@@ -1,9 +1,6 @@
 import { polylineClose } from "../polylineFunctions/polylineClose";
 import { polylineIsClosed } from "../polylineFunctions/polylineIsClosed";
 import { IPolygon } from "../types";
-import { _Allocator } from "./_Allocator";
-
-const SHARED_ALLOCATOR = new _Allocator();
 
 export function _asPolyline(polygon: IPolygon) {
   if (polygon.length === 0) {
@@ -11,8 +8,6 @@ export function _asPolyline(polygon: IPolygon) {
   } else if (polylineIsClosed(polygon)) {
     return polygon;
   } else {
-    const transfer = SHARED_ALLOCATOR.allocTempArray(polygon.length + 2);
-    polylineClose(polygon, transfer);
-    return transfer;
+    return polylineClose(polygon);
   }
 }
