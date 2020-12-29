82d18711af6382161a5accd2b2ec7851db63cf47
Alex Ryan <alexanderfryan@gmail.com>
2020-12-28 23:18:14 -1000

    Modernize mat2dFromRotation

--- src/__test__/mat2dFunctions/mat2dFromRotation.spec.ts
+++ src/__test__/mat2dFunctions/mat2dFromRotation.spec.ts
@@ -1,24 +1,16 @@
 import { mat2dFromRotation } from "../../mat2dFunctions/mat2dFromRotation";
-import { mat2dReset } from "../../mat2dFunctions/mat2dReset";
-import { expectMat2dEqualsApprox } from "../helpers";
+import { expectMat2dEqualsApprox, _mat2d } from "../helpers";
 
+const SQRT1_2 = Math.SQRT1_2;
+const PI = Math.PI;
 describe("mat2dFromRotation", () => {
-  it("0 => [1 0 0 1 0 0]", () => {
-    expectMat2dEqualsApprox(mat2dFromRotation(0), mat2dReset(1, 0, 0, 1, 0, 0));
-  });
-
-  it("π => [-1 0 0 -1 0 0]", () => {
-    expectMat2dEqualsApprox(mat2dFromRotation(Math.PI), mat2dReset(-1, 0, 0, -1, 0, 0));
-  });
-
-  it("3π/4 => [-√2/2 -√2/2 +√2/2 -√2/2 0 0]", () => {
-    expectMat2dEqualsApprox(
-      mat2dFromRotation((3 * Math.PI) / 4),
-      mat2dReset(-Math.SQRT1_2, -Math.SQRT1_2, Math.SQRT1_2, -Math.SQRT1_2, 0, 0),
-    );
-  });
-
-  it("atan2(-8, -6) => [-0.6 0.8 -0.8 -0.6 0 0]", () => {
-    expectMat2dEqualsApprox(mat2dFromRotation(Math.atan2(-8, -6)), mat2dReset(-0.6, 0.8, -0.8, -0.6, 0, 0));
+  it.each`
+    rot                   | result
+    ${0}                  | ${[1, 0, 0, 1, 0, 0]}
+    ${PI}                 | ${[-1, 0, 0, -1, 0, 0]}
+    ${(3 * PI) / 4}       | ${[-SQRT1_2, -SQRT1_2, SQRT1_2, -SQRT1_2, 0, 0]}
+    ${Math.atan2(-8, -6)} | ${[-0.6, 0.8, -0.8, -0.6, 0, 0]}
+  `("$rot => $result", ({ rot, result }) => {
+    expectMat2dEqualsApprox(mat2dFromRotation(rot), _mat2d(result));
   });
 });
