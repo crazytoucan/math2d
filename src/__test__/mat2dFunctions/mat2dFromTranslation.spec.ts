fc53073e139b1817d6ad2c6ac6abbd61613f65b9
Alex Ryan <alexanderfryan@gmail.com>
2020-12-28 23:19:44 -1000

    Modernize mat2dFromTranslation

--- src/__test__/mat2dFunctions/mat2dFromTranslation.spec.ts
+++ src/__test__/mat2dFunctions/mat2dFromTranslation.spec.ts
@@ -1,17 +1,13 @@
 import { mat2dFromTranslation } from "../../mat2dFunctions/mat2dFromTranslation";
-import { mat2dReset } from "../../mat2dFunctions/mat2dReset";
-import { expectMat2dEqualsApprox } from "../helpers";
+import { expectMat2dEqualsApprox, _mat2d } from "../helpers";
 
 describe("mat2dFromTranslation", () => {
-  it("0, 0 => [1 0 0 1 0 0]", () => {
-    expectMat2dEqualsApprox(mat2dFromTranslation(0, 0), mat2dReset(1, 0, 0, 1, 0, 0));
-  });
-
-  it("10, -20 => [1 0 0 1 10 -20]", () => {
-    expectMat2dEqualsApprox(mat2dFromTranslation(10, -20), mat2dReset(1, 0, 0, 1, 10, -20));
-  });
-
-  it("NaN, NaN => [1 0 0 1 NaN NaN]", () => {
-    expectMat2dEqualsApprox(mat2dFromTranslation(NaN, NaN), mat2dReset(1, 0, 0, 1, NaN, NaN));
+  it.each`
+    x      | y      | result
+    ${0}   | ${0}   | ${[1, 0, 0, 1, 0, 0]}
+    ${10}  | ${-20} | ${[1, 0, 0, 1, 10, -20]}
+    ${NaN} | ${NaN} | ${[1, 0, 0, 1, NaN, NaN]}
+  `("$x $y => $result", ({ x, y, result }) => {
+    expectMat2dEqualsApprox(mat2dFromTranslation(x, y), _mat2d(result));
   });
 });
