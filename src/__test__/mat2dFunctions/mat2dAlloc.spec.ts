91e135dcc7cf726b16d0a2df1ae681251ad0dd67
Alex Ryan <alexanderfryan@gmail.com>
2020-12-28 23:16:13 -1000

    Modernize mat2dAlloc, mat2dClone, mat2dDeterminant

--- src/__test__/mat2dFunctions/mat2dAlloc.spec.ts
+++ src/__test__/mat2dFunctions/mat2dAlloc.spec.ts
@@ -3,7 +3,7 @@ import { mat2dReset } from "../../mat2dFunctions/mat2dReset";
 import { expectMat2dEqualsApprox } from "../helpers";
 
 describe("mat2dAlloc", () => {
-  it("returns [NaN, NaN, NaN, NaN, NaN, NaN]", () => {
+  it("returns all NaNs", () => {
     expectMat2dEqualsApprox(mat2dAlloc(), mat2dReset(NaN, NaN, NaN, NaN, NaN, NaN));
   });
 });
--- src/__test__/mat2dFunctions/mat2dDeterminant.spec.ts
+++ src/__test__/mat2dFunctions/mat2dDeterminant.spec.ts
@@ -1,17 +1,13 @@
 import { mat2dDeterminant } from "../../mat2dFunctions/mat2dDeterminant";
-import { mat2dIdentity } from "../../mat2dFunctions/mat2dIdentity";
-import { mat2dReset } from "../../mat2dFunctions/mat2dReset";
+import { _mat2d } from "../helpers";
 
 describe("mat2dDeterminant", () => {
-  it("|1 0 0 1 0 0| => 1", () => {
-    expect(mat2dDeterminant(mat2dIdentity())).toBe(1);
-  });
-
-  it("|2 0 0 2 10 10| => 4", () => {
-    expect(mat2dDeterminant(mat2dReset(2, 0, 0, 2, 10, 10))).toBe(4);
-  });
-
-  it("|4 -7 8 5 NaN NaN| => 76", () => {
-    expect(mat2dDeterminant(mat2dReset(4, -7, 8, 5, NaN, NaN))).toBe(76);
+  it.each`
+    mat                        | result
+    ${[1, 0, 0, 1, 0, 0]}      | ${1}
+    ${[2, 0, 0, 2, 10, 10]}    | ${4}
+    ${[4, -7, 8, 5, NaN, NaN]} | ${76}
+  `("$mat => $result", ({ mat, result }) => {
+    expect(mat2dDeterminant(_mat2d(mat))).toBe(result);
   });
 });
