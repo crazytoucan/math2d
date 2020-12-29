7f6acb983dc2b66cc24c148d7a72a165d89e709e
Alex Ryan <alexanderfryan@gmail.com>
2020-12-28 22:39:03 -1000

    Modernize boxReset

--- src/__test__/boxFunctions/boxReset.spec.ts
+++ src/__test__/boxFunctions/boxReset.spec.ts
@@ -2,17 +2,17 @@ import { boxAlloc } from "../../boxFunctions/boxAlloc";
 import { boxReset } from "../../boxFunctions/boxReset";
 
 describe("boxReset", () => {
-  it("should copy components", () => {
-    const res = boxReset(4, 5, 6, 7);
-    expect(res.minX).toBe(4);
-    expect(res.minY).toBe(5);
-    expect(res.maxX).toBe(6);
-    expect(res.maxY).toBe(7);
+  it("sets components", () => {
+    expect(boxReset(4, 5, 6, 7)).toEqual({
+      minX: 4,
+      minY: 5,
+      maxX: 6,
+      maxY: 7,
+    });
   });
 
-  it("should return `out` if given", () => {
+  it("returns `out` if given", () => {
     const out = boxAlloc();
-    const res = boxReset(4, 5, 6, 7, out);
-    expect(res).toBe(out);
+    expect(boxReset(4, 5, 6, 7, out)).toBe(out);
   });
 });
