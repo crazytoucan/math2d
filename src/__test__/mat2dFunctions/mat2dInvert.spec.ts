04287d425e2b861fc21b25360fd6be95d66bd89f
Alex Ryan <alexanderfryan@gmail.com>
2020-12-28 23:20:58 -1000

    Consistent naming

--- src/__test__/mat2dFunctions/mat2dInvert.spec.ts
+++ src/__test__/mat2dFunctions/mat2dInvert.spec.ts
@@ -3,7 +3,7 @@ import { expectMat2dEqualsApprox, _mat2d } from "../helpers";
 
 describe("mat2dInvert", () => {
   it.each`
-    input                       | output
+    mat                         | result
     ${[1, 0, 0, 1, 0, 0]}       | ${[1, 0, 0, 1, 0, 0]}
     ${[1, 0, 0, 1, -10, 20]}    | ${[1, 0, 0, 1, 10, -20]}
     ${[0.5, 0, 0, -0.25, 0, 0]} | ${[2, 0, 0, -4, 0, 0]}
@@ -11,7 +11,7 @@ describe("mat2dInvert", () => {
     ${[4, 4, 4, 4, 6, 8]}       | ${[NaN, NaN, NaN, NaN, NaN, NaN]}
     ${[1, 0, 1, 0, 6, 8]}       | ${[NaN, NaN, NaN, NaN, NaN, NaN]}
     ${[3, 12, -4, -16, 0, 0]}   | ${[NaN, NaN, NaN, NaN, NaN, NaN]}
-  `("$input => $output", ({ input, output }) => {
-    expectMat2dEqualsApprox(mat2dInvert(_mat2d(input)), _mat2d(output));
+  `("$mat => $result", ({ mat, result }) => {
+    expectMat2dEqualsApprox(mat2dInvert(_mat2d(mat)), _mat2d(result));
   });
 });
