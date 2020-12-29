c36289daf811ba931d831d49f83990d65f46af39
Alex Ryan <alexanderfryan@gmail.com>
2020-12-28 23:29:07 -1000

    Modernize mat2dRotate

--- src/__test__/mat2dFunctions/mat2dRotate.spec.ts
+++ src/__test__/mat2dFunctions/mat2dRotate.spec.ts
@@ -1,27 +1,16 @@
-import { mat2dIdentity } from "../../mat2dFunctions/mat2dIdentity";
-import { mat2dReset } from "../../mat2dFunctions/mat2dReset";
 import { mat2dRotate } from "../../mat2dFunctions/mat2dRotate";
-import { expectMat2dEqualsApprox } from "../helpers";
+import { expectMat2dEqualsApprox, _mat2d } from "../helpers";
+
+const PI = Math.PI;
+const SQRT3 = Math.sqrt(3);
 
 describe("mat2dRotate", () => {
-  it("[1 0 0 1 0 0] rot π/3 => [1/2 -√3/2 √3/2 1/2 0 0]", () => {
-    expectMat2dEqualsApprox(
-      mat2dRotate(mat2dIdentity(), Math.PI / 3),
-      mat2dReset(0.5, -0.5 * Math.sqrt(3), 0.5 * Math.sqrt(3), 0.5, 0, 0),
-    );
-  });
-
-  it("[1 0 0 1 10 20] rot π/3 => [1/2 -√3/2 √3/2 1/2 10 20]", () => {
-    expectMat2dEqualsApprox(
-      mat2dRotate(mat2dReset(1, 0, 0, 1, 10, 20), Math.PI / 3),
-      mat2dReset(0.5, -0.5 * Math.sqrt(3), 0.5 * Math.sqrt(3), 0.5, 10, 20),
-    );
-  });
-
-  it("[1/2 -√3/2 √3/2 1/2 10 20] rot 2π/3 => [-1 0 0 -1 10 20]", () => {
-    expectMat2dEqualsApprox(
-      mat2dRotate(mat2dReset(0.5, -0.5 * Math.sqrt(3), 0.5 * Math.sqrt(3), 0.5, 10, 20), (2 * Math.PI) / 3),
-      mat2dReset(-1, 0, 0, -1, 10, 20),
-    );
+  it.each`
+    mat                                              | rot             | result
+    ${[1, 0, 0, 1, 0, 0]}                            | ${PI / 3}       | ${[0.5, -0.5 * SQRT3, 0.5 * SQRT3, 0.5, 0, 0]}
+    ${[1, 0, 0, 1, 10, 20]}                          | ${PI / 3}       | ${[0.5, -0.5 * SQRT3, 0.5 * SQRT3, 0.5, 10, 20]}
+    ${[0.5, -0.5 * SQRT3, 0.5 * SQRT3, 0.5, 10, 20]} | ${(2 * PI) / 3} | ${[-1, 0, 0, -1, 10, 20]}
+  `("$mat $rot => $result", ({ mat, rot, result }) => {
+    expectMat2dEqualsApprox(mat2dRotate(_mat2d(mat), rot), _mat2d(result));
   });
 });
