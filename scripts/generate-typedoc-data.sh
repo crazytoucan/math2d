c55366dbc2dd42d2e3b773acd90bf3a0854020d9
Alex Ryan <alexanderfryan@gmail.com>
2020-12-29 22:04:45 -1000

    Remove the docs/

--- scripts/generate-typedoc-data.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-typedoc  \
-  --json docs/_data/typedoc_out.json \
-  --excludeExternals \
-  --excludePrivate \
-  --externalPattern 'src/+(internal|__test__)/**/*.ts' \
-  --mode file \
-  --entryPoint 'index' \
-  --excludeNotExported
